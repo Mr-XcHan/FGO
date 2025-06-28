74c1d38265d33b9caf50fab361b0238e90941252
XcHan <isxinchen.han@gmail.com>
2025-06-28 15:50:38 +0200

    Update train_cewe_zr1.sh

--- train_cewe/train_cewe_zr1.sh
+++ train_cewe/train_cewe_zr1.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1                   # ⬅️ 每节点申请2个GPU
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=48:00:00
-#SBATCH --output=/mnt/beegfs/home/han/offline_rl/code/GPG/open-r1/logs/ZR1_CEWE_%j_%t.out
+#SBATCH --output=/mnt/fast/nobackup/scratch4weeks/ly0008/xch/code/CEWE_/logs/ZR1_CEWE_%j_%t.out
 
 # ✅ 1. 激活你的 Conda 环境
 source ~/.bashrc
