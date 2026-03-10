175e582a47134e9b89e0b69e3dba00d5829771e7
XcHan <isxinchen.han@gmail.com>
2026-03-10 20:40:29 +0100

    Update huggingface_up.py

--- huggingface_up.py
+++ huggingface_up.py
@@ -4,8 +4,8 @@ import os
 token = os.environ["HF_TOKEN"]
 
 
-username = "HHHHxc"
-repo_name = "DeepSeek-R1-Distill-Qwen-1.5B-CEWE"
+username = ""
+repo_name = ""
 repo_id = f"{username}/{repo_name}"
 
 # 创建私有模型仓库
@@ -13,7 +13,7 @@ create_repo(repo_id, repo_type="model", private=True, token=token)
 
 # 上传模型文件夹
 upload_folder(
-    folder_path="/mnt/fast/nobackup/scratch4weeks/ly0008/xch/code/CEWE_/output_logs/CEWE/GRPO/DeepSeek-R1-Distill-Qwen-1.5B/checkpoint-200",
+    folder_path="",
     repo_id=repo_id,
     repo_type="model",
     token=token
